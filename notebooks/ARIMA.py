3603f7f87d1f039dfd301f5f340aac2948fc4c1c
Rogelio Lopez <rolopez21@yahoo.com>
2019-11-18 20:08:35 -0600

    update LRLB fuction to PY

--- notebooks/ARIMA.py
+++ notebooks/ARIMA.py
@@ -5,4 +5,4 @@ def arima_fuc (df):
     model = ARIMA(df.VLO.values, order=(2, 1, 1))   
     results = model.fit()
     forecast = pd.DataFrame(results.forecast(steps=5)[0])
-    return results.summary(), forecast.plot(title="Futures Forecast")
+    return  forecast
